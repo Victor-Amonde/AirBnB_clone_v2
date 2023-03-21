7a50e8696a05ddaee4e612c9e49968b90193c4f8
Victor Amonde <victor.amonde@gmail.com>
2023-03-21 05:46:33 -0700

    MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,17 +4,9 @@
 -- User hbnb_dev with password hbnb_dev_pwd
 -- Grants all privileges for hbnb_dev on hbnb_dev_db.
 -- Grants SELECT privilege for hbnb_dev on performance_schema.
-
+-- Create dev database and user
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER
-       IF NOT EXISTS 'hbnb_dev'@'localhost'
-       IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES
-      ON `hbnb_dev_db`.*
-      TO 'hbnb_dev'@'localhost'
-      IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT
-      ON `performance_schema`.*
-      TO 'hbnb_dev'@'localhost'
-      IDENTIFIED BY 'hbnb_dev_pwd';
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
