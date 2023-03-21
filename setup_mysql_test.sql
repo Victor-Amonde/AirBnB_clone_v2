556cef35306d49837d75ce8f3821e02e9b0b5efa
Victor Amonde <victor.amonde@gmail.com>
2023-03-21 05:51:00 -0700

    MySQL setup test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,17 +3,9 @@
 -- password for hbnb_test set to hbnb_test_pwd
 -- hbnb_test should have all privileges on db hbnb_test_db
 -- grants hbnb_test SELECT privileges on database performance schema
-
+-- Create test database and user
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER
-       IF NOT EXISTS 'hbnb_test'@'localhost'
-       IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES
-      ON `hbnb_test_db`.*
-      TO 'hbnb_test'@'localhost'
-      IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT
-      ON `performance_schema`.*
-      TO 'hbnb_test'@'localhost'
-      IDENTIFIED BY 'hbnb_test_pwd';
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
