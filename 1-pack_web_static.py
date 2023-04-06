a57b2a3520ae0387a6349912ad54cb81b4729a53
Victor Amonde <victor.amonde@gmail.com>
2023-04-06 06:01:28 -0700

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
-"""
-file to practice use of Fabric
-"""
-from fabric.api import *
-import time
+# fabfile that generates a .tgz archive of web_static content
+import os.path
+from fabric.api import local
+from datetime import datetime
 
 
 def do_pack():
-    timestr = time.strftime("%Y%m%d%H%M%S")
-    try:
-        local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{}.tgz web_static/".
-              format(timestr))
-        return ("versions/web_static_{}.tgz".format(timestr))
-    except:
+    """
+    creates a .tgz archive of web_static contents
+    """
+    dt = datetime.utcnow()
+    file_name = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                              dt.month,
+                                                              dt.day,
+                                                              dt.hour,
+                                                              dt.minute,
+                                                              dt.second)
+    # create vetsions folder if it doesnt exist
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+
+    if local("tar -cvzf {} web_static".format(file_name)).failed is True:
         return None
+    return file_name
