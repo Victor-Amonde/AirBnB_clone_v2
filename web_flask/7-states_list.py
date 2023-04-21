9f939484442cda4a94a33ab8c8f6f3859e43d0f6
Victor-Amonde <victor.amonde@gmail.com>
2023-04-21 07:16:57 -0700

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,33 +1,31 @@
 #!/usr/bin/python3
+"""Start web application with two routings
 """
-starts a Flask web application
-manipulates storage
-"""
+
 from models import storage
-from flask import Flask
-from flask import render_template
-
-
+from models.state import State
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list')
 def states_list():
+    """Render template with states
     """
-    Displays a html page with a list of all state objects
-    in DBStorage
-    """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+    path = '7-states_list.html'
+    states = storage.all(State)
+    # sort State object alphabetically by name
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template(path, sorted_states=sorted_states)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """
-    Removes the current SQLAlchemy session
+def app_teardown(arg=None):
+    """Clean-up session
     """
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
