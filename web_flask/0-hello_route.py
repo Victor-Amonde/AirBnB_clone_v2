04e23e2f3fb2df43b62b338c3906a3e8729cd17b
Victor-Amonde <victor.amonde@gmail.com>
2023-05-20 12:37:44 +0000

    task no. 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,11 +5,11 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-@app.route('/airbnb-onepage/', strict_slashes=False)
+@app.route('/airbnb-onepage/')
 def hello():
     return 'Hello HBNB!'
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
     """print web"""
     return 'Hello HBNB!'
